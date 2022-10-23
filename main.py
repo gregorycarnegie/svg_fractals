f08b7dbd64879d43f809d8239899ab75458abb42
Gregory <36880726+gregorycarnegie@users.noreply.github.com>
2022-10-23 10:32:37 +0100

    initial commit to github

--- main.py
+++ main.py
@@ -1,16 +1,129 @@
-# This is a sample Python script.
+from math import sqrt
+import numpy as np
+import random
+import svgwrite
+import utils
+import pandas as pd
+import quantumrandom as qr
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+"""DATA"""
+colours = pd.read_excel('DATA.xlsx', sheet_name='Sheet1')
+colour_list = np.array(utils.ratio(colours['Colour Names'], colours['Numbers']))
+colour_names = colours['SVG name']
+patterns = pd.read_excel('DATA.xlsx', sheet_name='Sheet2')
+pattern_list = np.array(utils.ratio(patterns['Pattern'], patterns['Numbers']))
+
+phi = (1 + sqrt(5)) / 2
+length = 1e3
+svg_size_w = svg_size_h = length
+length_array = np.array([1.0] + [length / n for n in range(1, 101)])
+eye_radius = 0.5 * (phi - 1) * length
+eye_sqrt2 = eye_radius / sqrt(2)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def run(file_name, choice):
+    file_name = f'svgs\\{file_name}.svg'
+    if choice in {'y', 'yes'}:
+        random.seed(qr.randint(1, 5_000_000_000))
+        """Canvas"""
+        # ############################################################################################################ #
+        dwg = svgwrite.Drawing(file_name, (svg_size_w, svg_size_h), profile='full', debug=True)
+        dwg.viewbox(-svg_size_w / 2, -svg_size_h / 2, svg_size_w, svg_size_h)
+        # ############################################################################################################ #
+        """Background"""
+        background_fill = random.choice(colour_list[:-20])
+        dwg.add(dwg.rect(insert=(-length_array[2], -length_array[2]), size=('100%', '100%'), fill=background_fill))
+        # ############################################################################################################ #
+        """Pattern Choices"""
+        pattern_choice = random.choice(pattern_list)
+        pattern_choice_col = [x for x in colour_list[:-20] if x != background_fill]
+        if pattern_choice == 'Hilbert':
+            iterations = random.randint(1, 5)
+            pl = dwg.polyline(points=utils.hilbert(length, iterations), fill='none',
+                              stroke_width=int(length / (20 * (iterations + 1))),
+                              stroke=random.choice(pattern_choice_col))
+            dwg.add(pl)
+        elif pattern_choice == 'Gosper':
+            iterations = 3
+            pl = dwg.polyline(points=utils.gosper(5 * length / 4, iterations, 5 * length / 4, 0), fill='none',
+                              stroke_width=int(length / (20 * (iterations + 1))),
+                              stroke=random.choice(pattern_choice_col))
+            dwg.add(pl)
+        elif pattern_choice == 'Peano':
+            iterations = random.randint(1, 3)
+            pl = dwg.polyline(points=utils.peano(length, iterations), fill='none',
+                              stroke_width=int(length / (40 * (iterations + 1))),
+                              stroke=random.choice(pattern_choice_col))
+            dwg.add(pl)
+        if pattern_choice == 'Moore':
+            iterations = random.randint(0, 3)
+            pl = dwg.polyline(points=utils.moore(length, iterations), fill='none',
+                              stroke_width=int(length / (20 * (iterations + 1))),
+                              stroke=random.choice(pattern_choice_col))
+            dwg.add(pl)
+    else:
+        iterations = int(input('How many iterations do you need?'))
+        """Canvas"""
+        # ############################################################################################################ #
+        dwg = svgwrite.Drawing(file_name, (svg_size_w, svg_size_h), profile='full', debug=True)
+        dwg.viewbox(-svg_size_w / 2, -svg_size_h / 2, svg_size_w, svg_size_h)
+        # ############################################################################################################ #
+        """Background"""
+        background_fill = input('What colour background do you want?')
+        dwg.add(dwg.rect(insert=(-length_array[2], -length_array[2]), size=('100%', '100%'), fill=background_fill))
+        # ############################################################################################################ #
+        """Pattern Choices"""
+        pattern_choice = input(
+            """
+            Please choose from the below list:
+            - Hilbert
+            - Gosper
+            - Moore
+            - Peano
+            """
+        ).lower()
+        pattern_choice_col = input('What colour do you want the pattern to be?')
+        loop = True
+        while loop:
+            if pattern_choice_col == background_fill:
+                escape = input('The pattern and the background are the same colour\nAre you sure this is what you want?')
+                if escape.lower() in {'y', 'yes'}:
+                    loop = False
+                else:
+                    background_fill = input('What colour background do you want?')
+                    pattern_choice_col = input('What colour do you want the pattern to be?')
+                    if pattern_choice_col == background_fill:
+                        escape = input('The pattern and the background are the same colour\nAre you sure this is what you want?')
+                        if escape.lower() in {'y', 'yes'}:
+                            loop = False
+            else:
+                loop = False
+
+        if pattern_choice == 'hilbert':
+            pl = dwg.polyline(points=utils.hilbert(length, iterations), fill='none',
+                              stroke_width=int(length / (20 * (iterations + 1))),
+                              stroke=pattern_choice_col)
+            dwg.add(pl)
+        elif pattern_choice == 'gosper':
+            pl = dwg.polyline(points=utils.gosper(5 * length / 4, iterations, 5 * length / 4, 0), fill='none',
+                              stroke_width=int(length / (20 * (iterations + 1))),
+                              stroke=pattern_choice_col)
+            dwg.add(pl)
+        elif pattern_choice == 'peano':
+            pl = dwg.polyline(points=utils.peano(length, iterations), fill='none',
+                              stroke_width=int(length / (40 * (iterations + 1))),
+                              stroke=pattern_choice_col)
+            dwg.add(pl)
+        if pattern_choice == 'moore':
+            pl = dwg.polyline(points=utils.moore(length, iterations), fill='none',
+                              stroke_width=int(length / (20 * (iterations + 1))),
+                              stroke=pattern_choice_col)
+            dwg.add(pl)
+
+    dwg.save()
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    filename = input('Please type the name of your file:')
+    random_select = input('Print a random pattern (Yes/No?):').lower()
+    run(filename, random_select)
---
+++ utils.py
@@ -0,0 +1,132 @@
+import re
+from math import cos, sin, pi
+import numpy as np
+
+
+def colour_proper(proper_names, names, colour):
+    return proper_names[np.where(names == colour)[0][0]]
+
+
+def ratio(names, numbers):
+    ratio_list = []
+    for n in range(len(names)):
+        ratio_list += [names[n] for _ in range(int(numbers[n]))]
+    return ratio_list
+
+
+def lsystem(axioms, rules, iterations):
+    """We iterate through our method required numbers of time."""
+    for _ in range(iterations):
+        """Our newly created axioms from this iteration."""
+        newAxioms = ''.join(rules[axiom] if axiom in rules else axiom for axiom in axioms)
+        """
+        You will need to iterate through your newAxioms next time, so...
+        We transfer newAxioms, to axioms that is being iterated on, in the for loop.
+        """
+        axioms = newAxioms
+    return axioms
+
+
+def reduce_instructions(x, y, z, mult, constant, sequence, theta, A, dtype):
+    sequence = re.sub('F' * mult[2][0], mult[2][1], sequence)
+    sequence = re.sub('F' * mult[1][0], mult[1][1], sequence)
+    sequence = re.sub('F' * mult[0][0], mult[0][1], sequence)
+
+    n = 0
+    if dtype in {int, np.int}:
+        for char in sequence:
+            if char == '+':
+                n += theta
+            if char == mult[0][1]:
+                a = np.array([cos(n), sin(n)]).astype(int)
+                x += mult[0][0] * constant * np.dot(a, A)
+                y -= mult[0][0] * constant * np.dot(np.fliplr([a])[0], A)
+                z = np.append(z, [[x, y]], axis=0)
+            if char == mult[1][1]:
+                a = np.array([cos(n), sin(n)]).astype(int)
+                x += mult[1][0] * constant * np.dot(a, A)
+                y -= mult[1][0] * constant * np.dot(np.fliplr([a])[0], A)
+                z = np.append(z, [[x, y]], axis=0)
+            if char == mult[2][1]:
+                a = np.array([cos(n), sin(n)]).astype(int)
+                x += mult[2][0] * constant * np.dot(a, A)
+                y -= mult[2][0] * constant * np.dot(np.fliplr([a])[0], A)
+                z = np.append(z, [[x, y]], axis=0)
+            if char == '_':
+                n -= theta
+    else:
+        for char in sequence:
+            if char == '+':
+                n += theta
+            if char == mult[0][1]:
+                a = np.array([cos(n), sin(n)])
+                x += mult[0][0] * constant * np.dot(a, A)
+                y -= mult[0][0] * constant * np.dot(np.fliplr([a])[0], A)
+                z = np.append(z, [[x, y]], axis=0)
+            if char == mult[1][1]:
+                a = np.array([cos(n), sin(n)])
+                x += mult[1][0] * constant * np.dot(a, A)
+                y -= mult[1][0] * constant * np.dot(np.fliplr([a])[0], A)
+                z = np.append(z, [[x, y]], axis=0)
+            if char == mult[2][1]:
+                a = np.array([cos(n), sin(n)])
+                x += mult[2][0] * constant * np.dot(a, A)
+                y -= mult[2][0] * constant * np.dot(np.fliplr([a])[0], A)
+                z = np.append(z, [[x, y]], axis=0)
+            if char == '_':
+                n -= theta
+    print(sequence)
+    return z
+
+
+def hilbert(step_length, order):
+    x = step_length * (pow(0.5, order + 1) - 0.5)
+    y = step_length * (0.5 - pow(0.5, order + 1))
+    constant = step_length * (pow(0.5, order))
+    z = np.array([[x, y]])
+
+    sequence = lsystem('A', {'A': '+BF_AFA_FB+', 'B': '_AF+BFB+FA_'}, order)
+    sequence = re.sub('[+_][_+]', '', sequence)
+    sequence = re.sub('[AB]', '', sequence)
+
+    z = reduce_instructions(x, y, z, ((1, 'A'), (2, 'B'), (3, 'C')), constant, sequence, 0.5 * pi, (1, 0), int)
+    return z
+
+
+def gosper(step_length, order, x, y):
+    const = step_length * (pow(0.5, 3))
+    z = np.array([[x, y]])
+
+    sequence = lsystem('A', {'A': 'A_B__B+A++AA+B_', 'B': '+A_BB__B_A++A+B'}, order)
+    sequence = re.sub('[AB]', 'F', sequence)
+
+    z = reduce_instructions(x, y, z, ((1, 'A'), (2, 'B'), (3, 'C')), const, sequence, pi/3, (0, 1), float)
+    return z
+
+
+def peano(step_length, order):
+    x = y = 0.5 * step_length * 0.95
+    constant = 0.95 * step_length / (pow(3, order) - 1)
+    z = np.array([[x, y]])
+
+    sequence = lsystem('L', {'L': 'LFRFL_F_RFLFR+F+LFRFL', 'R': 'RFLFR+F+LFRFL_F_RFLFR'}, order)
+    sequence = re.sub('[+_][_+]', '', sequence)
+    sequence = re.sub('[LR]', '', sequence)
+
+    z = reduce_instructions(x, y, z, ((1, 'A'), (2, 'B'), (5, 'C')), constant, sequence, 0.5 * pi, (0, 1), int)
+    return z
+
+
+def moore(step_length, order):
+    constant = step_length / (pow(2, order + 1))
+    x = -constant / 2
+    y = (step_length - constant) / 2
+
+    z = np.array([[x, y]])
+
+    sequence = lsystem('LFL+F+LFL', {'L': '_RF+LFL+FR_', 'R': '+LF_RFR_FL+'}, order)
+    sequence = re.sub('[+_][_+]', '', sequence)
+    sequence = re.sub('[LR]', '', sequence)
+
+    z = reduce_instructions(x, y, z, ((1, 'A'), (2, 'B'), (3, 'C')), constant, sequence, 0.5 * pi, (0, 1), int)
+    return z
